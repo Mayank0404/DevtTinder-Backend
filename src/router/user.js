71879e16b1bf9032c96f43504f33ba84a9239cbc
mayank0404 <mayanksharma040404@gmail.com>
2025-01-23 11:01:40 +0530

    made the feed api with paginatin included

--- src/router/user.js
+++ src/router/user.js
@@ -54,6 +54,70 @@ userRouter.get("/user/connections",userAuth, async(req,res)=>{
     }
 })
 
+// 
+// userRouter.get("/user/feed",userAuth,async(req,res)=>{
+//     try {
+//         const loggedInUser=req.user;
 
+//         const connectionRequest=await ConnectionRequest.find({
+//             $or:[
+//                 {fromUserId:loggedInUser._id},
+//                 {toUserId:loggedInUser._id}
+//             ]
+//         }).select("fromUserId toUserId")
+
+//         const hideFromFeed=new Set();
+//         connectionRequest.forEach((req)=>{
+//             hideFromFeed.add(req.toUserId.toString());
+//             hideFromFeed.add(req.fromUserId.toString());
+//         })
+
+//         const users=await User.find({
+//             $and:[
+//                 {_id:{$nin: Array.from(hideFromFeed)}},
+//                 {_id:{$ne:loggedInUser._id}},
+//             ],
+//         }).select(USER_PUBLIC_DATA);     
+        
+//         res.send(users);
+//     } catch (error) {
+//         res.status(400).send("ERROR "+error.message);
+//     }
+// })
+userRouter.get("/user/feed",userAuth,async(req,res)=>{
+    try {
+       const loggedInUser=req.user;
+       const page=parseInt(req.query.page) || 1;
+       let limit=parseInt(req.query.limit) || 10;
+       limit=limit>50?50:limit;
+       const skip=(page-1)*limit;
+       const connectionRequest=await ConnectionRequest.find({
+        $or:[
+            {toUserId:loggedInUser._id},
+            {fromUserId:loggedInUser._id}
+        ]
+       }).select("fromUserId toUserId")
+
+       const hideFromFeed=new Set();
+       connectionRequest.forEach((req)=>{
+        hideFromFeed.add(req.toUserId.toString());
+        hideFromFeed.add(req.fromUserId.toString());
+       })
+       
+       const users=await User.find({
+        $and:[
+            {_id:{$nin : Array.from(hideFromFeed)}},
+            {_id:{$ne:loggedInUser._id}}
+        ]
+       })
+       .select(USER_PUBLIC_DATA)
+       .skip(skip)
+       .limit(limit);
+       res.send(users);
+
+    } catch (error) {
+        res.status(400).send("ERROR "+error.message);
+    }
+})
 
 module.exports=userRouter; 
